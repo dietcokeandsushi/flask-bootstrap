5268b7b5e5750f728d9b70437ac760b005f0ef6f
V <ztingmike@gmail.com>
2026-01-06 11:21:01 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, redirect, url_for
+ 
+app = Flask(__name__)
+
+cars = [
+   {'id':1, 'brand':'Toyota','model':'Taris Ativ','year':2024, 'price': 560000},
+   {'id':2, 'brand':'Toyota','model':'Taris Cross','year':5, 'price': 860000},
+   {'id':3, 'brand':'Nissan','model':'GTR','year':2024, 'price': 1560000}
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Home Page')
+
+@app.route('/cars')
+def show_cars():
+    return render_template('cars/cars.html', 
+    title='Show All Cars Page', cars=cars)
+
+@app.route('/cars/new', methods=['GET', 'POST'])
+def new_car():
+    if request.method == 'POST':
+      brand = request.form['brand']
+      model = request.form['model']
+      year = int(request.form['year'])
+      price = int(request.form['price'])
+      
+      length = len(cars)
+      id =cars[length - 1]['id'] + 1
+      car = {'id':id, 'brand':brand,'model':model,'year':year, 'price': price}
+      
+      cars.append(car)
+      return redirect(url_for('show_cars'))
+      
+    return render_template('cars/new_car.html', 
+    title='New Car Page')
